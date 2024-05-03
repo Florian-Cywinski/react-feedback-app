80cf391ebb5ce357d84d62b9c226ccd617f877f1
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-03 16:29:38 +0200

    3 - Components, Props & State -> 3 - State & useState Hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
     return (
       <>    {/* This is used to be able to bring in both <Header /> and <div> */}
-<<<<<<< HEAD
         {/* <Header text='Hello World' />  To bring in the imported component - text='Hello World' is a prop (property) which can be past in to a component (see Header.jsx -> here the prop is catched) */}
         {/* <Header bgColor='red' textColor='blue' /> */}
         <Header />
-=======
-        <Header text='Hello World' />  {/* To bring in the imported component - text='Hello World' is a prop (property) which can be past in to a component (see Header.jsx -> here the prop is catched) */}
->>>>>>> 6e30c4e84af191e80bc3888dc304fe5c46f48588
         <div className='container'>
-          <h1>My App</h1>
+          <FeedbackItem />
         </div>
       </>
     )
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+function FeedbackItem() {
+  const [rating, setRating] = useState(7)
+  const [text, setText] = useState('This is an example of a feedback item')
+//   const handleClick = () => setRating(rating + 1)
+const handleClick = () => setRating(prev => { return prev + 1 })    // The same as one line before
+
+  return (
+    <div className="card">
+        <div className="num-display">{rating}</div>
+        <div className="text-display">{text}</div>
+        <button onClick={handleClick}>Click</button>
+    </div>
+  )
+}
+
+export default FeedbackItem
