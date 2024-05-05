7f6e0f1350a8eab825bb8f788b986eb948d83b20
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-05 14:19:05 +0200

    4 - Forms, Validation & Simple Animation -> 4 - Rating Selection Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState, useContext, useEffect } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')  // To have a min. of 10 char in the input field filled in
 
@@ -28,6 +30,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} /> {/* select is a prop which is a function (it gets the selected value (rating) (1...10) from RatingSelect.jsx (select) and save it into setRating) */}
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text}/>
           <Button type='submit' isDisabled={btnDisabled}>Send</Button>
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+function RatingSelect({ select }) { // select is a prop which is a function (FeedbackForm.jsx)
+  const [selected, setSelected] = useState(10)  // see down below checked={selected === i + 1}
+  // NOTE: We don't need local state here as it's a duplicate of parent state
+  // also no real need for useEffect or context
+  // useEffect(() => {
+  //   select(feedbackEdit.item.rating)
+  // }, [feedbackEdit])
+
+  const handleChange = (e) => {
+    setSelected(+e.currentTarget.value) // to set (1...10) 
+    select(+e.currentTarget.value)  // to call the function select
+  }
+
+  // NOTE: simplified with iteration
+  return (
+    <ul className='rating'>
+      {Array.from({ length: 10 }, (_, i) => ( // To have 10 (1 to 10) rating circle buttons (to chose one)
+        <li key={`rating-${i + 1}`}>
+          <input
+            type='radio'
+            id={`num${i + 1}`}
+            name='rating'
+            value={i + 1}
+            onChange={handleChange} // handleChange is an event handeler
+            checked={selected === i + 1}  // if it matches it is set to true and therefor shown as the selected one
+          />
+          <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default RatingSelect
