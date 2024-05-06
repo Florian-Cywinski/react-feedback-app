5ecb8b530396d3d82dae938bac42d44e39ada2bc
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-06 19:25:56 +0200

    6 - Context API, useContext & Deployment -> 1 - Create a Context & Provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackForm from "./components/FeedbackForm"
 import FeedbackData from "./data/FeedbackData"
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
+import { FeedbackProvider } from './context/FeedbackContext'  // {} because it's not a default export
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -24,28 +25,30 @@ function App() {
     }
 
     return (
-      <Router>    {/* This is used to be able to bring in both <Header /> and <div> */}
-        {/* <Header text='Hello World' />  To bring in the imported component - text='Hello World' is a prop (property) which can be past in to a component (see Header.jsx -> here the prop is catched) */}
-        {/* <Header bgColor='red' textColor='blue' /> */}
-        <Header />
-        <div className='container'>
-          <Routes>
-            {/* To show the homepage - without exact it would have a conflict with /about */}
-            <Route exact path='/' element={    
-              <>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-                <AboutIconLink />
-              </>
-            }>
-            </Route>
+      <FeedbackProvider>
+        <Router>    {/* This is used to be able to bring in both <Header /> and <div> */}
+          {/* <Header text='Hello World' />  To bring in the imported component - text='Hello World' is a prop (property) which can be past in to a component (see Header.jsx -> here the prop is catched) */}
+          {/* <Header bgColor='red' textColor='blue' /> */}
+          <Header />
+          <div className='container'>
+            <Routes>
+              {/* To show the homepage - without exact it would have a conflict with /about */}
+              <Route exact path='/' element={    
+                <>
+                  <FeedbackForm handleAdd={addFeedback}/>
+                  <FeedbackStats feedback={feedback}/>
+                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                  <AboutIconLink />
+                </>
+              }>
+              </Route>
 
-            <Route path='/about' element={<AboutPage/>} />
-          </Routes>
-          {/* <AboutIconLink /> */}
-        </div>
-      </Router>
+              <Route path='/about' element={<AboutPage/>} />
+            </Routes>
+            {/* <AboutIconLink /> */}
+          </div>
+        </Router>
+      </FeedbackProvider>
     )
   }
   
