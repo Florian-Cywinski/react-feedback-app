1c8900ef4c277fafbeca1f6c2775eb1bb63bf659
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-06 17:21:26 +0200

    5 - Creating Routes & Links -> 1 - Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'   // To use this package just call uuidv4 as function to create unique ids
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackData from "./data/FeedbackData"
+import AboutPage from './pages/AboutPage'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,16 +23,20 @@ function App() {
     }
 
     return (
-      <>    {/* This is used to be able to bring in both <Header /> and <div> */}
+      <Router>    {/* This is used to be able to bring in both <Header /> and <div> */}
         {/* <Header text='Hello World' />  To bring in the imported component - text='Hello World' is a prop (property) which can be past in to a component (see Header.jsx -> here the prop is catched) */}
         {/* <Header bgColor='red' textColor='blue' /> */}
         <Header />
         <div className='container'>
-          <FeedbackForm handleAdd={addFeedback}/>
-          <FeedbackStats feedback={feedback}/>
-          <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+          <Route exact path='/'>    {/* To show the homepage - without exact it would have a conflict with /about */}
+            <FeedbackForm handleAdd={addFeedback}/>
+            <FeedbackStats feedback={feedback}/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+          </Route>
+
+          <Route path='/about' component={AboutPage} />
         </div>
-      </>
+      </Router>
     )
   }
   
